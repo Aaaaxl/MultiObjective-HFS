69f10c8e9fb41bce2676e18127d385ef1e50f5ae
Aaaaxl <146303338+Aaaaxl@users.noreply.github.com>
2025-05-21 15:51:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ DATA_DIR = os.path.join(BASE_DIR, 'data')
 RESULT_DIR = os.path.join(BASE_DIR, 'results')
 
 # Breast Cancer Data Path
-breast_raw_data_path = r'C:\Users\26494\PycharmProjects\MOO-HFS\dataset\Breast_Cancer'
+breast_raw_data_path = r'your_path\Breast_Cancer'
 
 breast_expression_probe = os.path.join(DATA_DIR, "Breast_Cancer/expression_matrix_probe.csv")
 
@@ -16,7 +16,7 @@ breast_data_path = os.path.join(DATA_DIR, "Breast_Cancer/Breast_Cancer.csv")
 breast_importances_path = os.path.join(DATA_DIR, "importances/Breast_Cancer")
 
 # Leukemia Data Path
-leukemia_raw_data_path = r'C:\Users\26494\PycharmProjects\MOO-HFS\dataset\Leukemia'
+leukemia_raw_data_path = r'your_path\Leukemia'
 
 leukemia_expression_path = os.path.join(DATA_DIR, 'Leukemia\expression_matrix.csv')
                                         
@@ -29,7 +29,7 @@ leukemia_data_path = os.path.join(DATA_DIR, 'Leukemia\leukemia.csv')
 leukemia_importances_path = os.path.join(DATA_DIR, "importances/Leukemia")
 
 # p53_Mutants
-p53_raw_data_path = r'C:\Users\26494\PycharmProjects\MOO-HFS\dataset\p53_Mutants\p53_old_2010'
+p53_raw_data_path = r'your_path\p53_old_2010'
 
 p53_data_path = os.path.join(DATA_DIR, 'p53_Mutants/p53_Mutants.csv')
 
@@ -37,7 +37,7 @@ p53_importances_path = os.path.join(DATA_DIR, 'importances/p53_Mutants')
 
 
 # Arrhythmia Data Path
-arrhythmia_raw_data_path = r'C:\Users\26494\PycharmProjects\MOO-HFS\dataset\Arrhythmia'
+arrhythmia_raw_data_path = r'your_path\Arrhythmia'
 
 arrhythmia_data_path = os.path.join(DATA_DIR, 'arrhythmia/arrhythmia.csv')
 
