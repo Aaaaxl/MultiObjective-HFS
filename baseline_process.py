21eff911d4295c9ce5411b37f3cdd720813e5e59
Aaaaxl <146303338+Aaaaxl@users.noreply.github.com>
2025-05-21 17:26:50 +0800

    Add files via upload

--- baseline_process.py
+++ baseline_process.py
@@ -3,7 +3,7 @@ from eval.eval_reduced import eval_reduced_datasets
 from config import *
 from utils import *
 
-def baseline_process(run_id, data_path, output_dir, importances_path,
+def baseline_process(data_path, output_dir, importances_path,
                      cv_mode='kfold', k=5, feature_n_config=[-1], clfs=None, random_states=[42]):
     
     for seed in random_states:
@@ -11,10 +11,10 @@ def baseline_process(run_id, data_path, output_dir, importances_path,
         feature_selection_all(data_path, 'Class', importances_path, seed)
 
         logging.info(f"===={seed} SELECT FINISHED !!! ===")
-
+    
         eval_importances_path = os.path.join(importances_path, str(seed))
         
-        eval_reduced_datasets(run_id, data_path, eval_importances_path, output_dir, 
+        eval_reduced_datasets(data_path, eval_importances_path, output_dir, 
                               cv_mode, k, feature_n_config, clfs, seed)
 
         logging.info(f"===={seed} EVAL FINISHED !!! ====")
